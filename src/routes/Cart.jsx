3fa4ba00c3d23e7382e16b676cc5687efdf9be94
prisha <prishapg101@gmail.com>
2023-04-30 23:26:19 +0530

    minnor changes

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,9 +1,21 @@
 import "./Cart.scss";
+
 import Topbar from "../components/Topbar";
+
 export default function Cart() {
+
+  
   return (
-    <div>
+    <div className="CartComponent">
       <Topbar></Topbar>
+      <div className="body">
+        <div className="left">
+          <div className="card">
+            img
+          </div>
+        </div>
+        <div className="right">reciept</div>
+      </div>
     </div>
   );
 }
